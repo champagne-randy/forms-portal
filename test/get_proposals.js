4573da5f5369b2483939bd4f14db981b0e197bea
Randy Champagne <r-champagne@neiu.edu>
2016-05-08 19:45:59 -0400

    continue implementing bdd test cases
    - added a mock proposals model
    - promisified each test case

--- test/get_proposals.js
+++ test/get_proposals.js
@@ -1,13 +1,41 @@
 'use strict';
 
 var chai 	= require('chai'),
-	expect 	= chai.expect;
+	expect 	= chai.expect,
+	Q		= require('q');
 	chai.use( require("sinon-chai") );
 	chai.use( require("chai-as-promised") );
 
 
 describe( 'GET /proposals', function() {
 	context('1 or more proposals were submitted', function() {
+		beforeEach(function() {
+			this.proposalsModel = [
+				{
+					_id: 1,
+					firstName: "Jane",
+					lastName: "Doe",
+					email: "a@b.com",
+					phone: "123456789",
+					title: "Awesome Program",
+					categoryID: 1,
+					description: "The most wholesome program to ever hit the airwaves"
+				},
+				{
+					_id: 2,
+					firstName: "John",
+					lastName: "Doe",
+					email: "c@d.com",
+					phone: "123456789",
+					title: "Serious Program",
+					categoryID: 4,
+					description: "The most important program to ever hit the airwaves"
+				}
+			];
+			this.proposalsSystem.display
+				.returns(Q.fulfill(this.proposalsModel));
+		});
+
 		it('will respond with a 200 code');
 
 		describe('will respond with an array of JSON objects', function() {
@@ -17,10 +45,30 @@ describe( 'GET /proposals', function() {
 
 
 	context('No proposals were submitted', function() {
-		// TODO
+		beforeEach(function() {
+			this.proposalsSystem.display
+				.returns(Q.fulfill(null));
+		});
+
+		it('will respond with a 404 code');
 	});
 
+
 	context('The forms web service is down', function() {
+		beforeEach(function() {
+			this.proposalsSystem.display
+				.returns(Q.reject(new Error('Expected error')));
+		});
+
 		it('will respond with a 500 code');
 	});
 });
+
+
+
+
+
+
+
+
+
